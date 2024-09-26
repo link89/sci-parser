86f0e3f9757340f91ca9a26f905eb87d0e222f12
weihong.xu <xuweihong.cn@gmail.com>
2024-09-26 22:34:45 +0800

    wip: implement parser

--- sci_parser/parser.py
+++ sci_parser/parser.py
@@ -3,19 +3,16 @@ import re
 class Parser:
 
     def __init__(self, fmt_fn=None, flags=0):
-
         self._match_fns = []
         self._default_fmt_fn = fmt_fn
         self._default_flags = flags
 
     def kv_search(self, key, value=r'(\S+)', sep=':', endwith='', fmt_fn=None, flags=None):
         key = re.escape(key)
-        endwith = re.escape(endwith)
         pattern = rf'\s*({key})\s*{sep}\s*{value}{endwith}'
         if fmt_fn is None and self._default_fmt_fn is None:
             fmt_fn = lambda m: m.groups()
         return self.re_search(pattern, fmt_fn, flags)
-    
         
     def re_search(self, pattern, fmt_fn=None, flags=None):
         if fmt_fn is None:
@@ -31,6 +28,5 @@ class Parser:
         for match_fn, fmt_fn in self._match_fns:
             match = match_fn(text)
             if match:
-                text = text[match.end():]  # this is not efficient
+                text = text[match.end():]
                 yield match if fmt_fn is None else fmt_fn(match)
-            
